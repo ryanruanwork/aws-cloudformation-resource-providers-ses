d92daa31c96c6c77aac1385ba382e31110ff482d
Jiahui (Ryan) Ruan <ryanruan@amazon.com>
2020-10-23 21:29:41 +0000

    Init resource handlers

---
+++ aws-lotus-frontendpackage/src/main/java/software/amazon/lotus/frontendpackage/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.lotus.frontendpackage;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-lotus-frontendpackage.json");
+    }
+}
